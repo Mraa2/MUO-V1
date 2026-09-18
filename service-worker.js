1fedc2092686e4db1d59dd014271a1738dd74e6a
Mraa2 <m.vevoda14@gmail.com>
2026-09-18 19:24:40 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,8 @@
 self.addEventListener("install",(event)=>{
 	event.waitUntil(
 		caches.open("pwa-cache").then((cache)=>{
-			console.log(cache, caches);
 			return cache.addAll([
-				"/MUO-V1/",
+				"/",
 				"/index.html",
 				"/style.css",
 				"/script.js",
