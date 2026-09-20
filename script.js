96e2f680a6c83ed75ce07fbd671f377007f5af24
Mraa2 <m.vevoda14@gmail.com>
2026-09-20 22:57:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -619,7 +619,7 @@ function addNewTopic(create) {
 	tablesCreated = false;
 	checkIsGreen = false;
 
-	checkButton.innerHTML = `<img src="icons/circle-check-regular-full.svg" class="sizefive" id="hablahabla" style="color: red;" alt=""></img>`
+	checkButton.innerHTML = <img src="icons/circle-check-regular-full.svg" id="hablahabla" class="sizefive"></img>`
 
 	if (!create) {
 		console.log(createdFile, currentId);
@@ -643,11 +643,11 @@ function isCheckAvailable(){
 	if ((tablesCreated) && (curName) && (!topics[curName]) && (curName !== "")) {
 		checkIsGreen = true;
 
-		checkButton.innerHTML = `<img src="icons/circle-check-regular-full.svg" class="sizefive" id="hablahabla" style="color: green;" alt=""></img>`
+		checkButton.innerHTML = `<img src="icons/circle-check-regular-full.svg" id="hablahabla" class="sizefive"></img>`
 	} else {
 		checkIsGreen = false;
 
-		checkButton.innerHTML = `<img src="icons/circle-check-regular-full.svg" class="sizefive" id="hablahabla" style="color: red;" alt=""></img>`
+		checkButton.innerHTML = `<img src="icons/circle-check-regular-full.svg" id="hablahabla" class="sizefive"></img>`
 	}
 }
 
